004eaea56d77392da31efa23a4f58f7ac5284b1b
Nuria-16cr <nuria.16cr@gmail.com>
2025-04-16 11:37:49 +1000

    Add files via upload

---
+++ IntMedTrial/IntMedTrial.js
@@ -0,0 +1,21 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+background(255, 200, 0);
+//frameRate(5);
+}
+
+
+function draw() {
+  //background(255, 200, 0,);
+  fill(0);
+  rect(mouseX, mouseY, 100, 200);
+  
+stroke(255);
+strokeWeight(4);
+textSize(36);
+//line(mouseX, mouseY, width/2, height/2);
+
+//text("0 0", mouseX, mouseY);
+
+
+}
