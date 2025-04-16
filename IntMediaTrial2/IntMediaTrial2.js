bc8d39fe0d6cda0c8f4b958efe5e58a0460d55d0
Nuria-16cr <nuria.16cr@gmail.com>
2025-04-16 11:24:58 +1000

    Add files via upload

---
+++ IntMediaTrial2/IntMediaTrial2.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas(windowWidth, windowHeight);
+background(255);
+noStroke();
+}
+
+
+function draw() {
+  for (let i=0; i<50; i++){
+    fill(random(255), random(255), random(255));
+    circle(random(width), random(height), random(40, 200));
+    triangle(random(width), random(height), random(width), random(height), random(height), random(40, 200));
+}
+noLoop();
+}
